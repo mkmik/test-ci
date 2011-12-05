8a43129b43979bfdb12670d468a42474bbefbb47
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2011-12-05 18:06:49 +0100

    Testing msi

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name='test-ci',
+    url='http://github.com/mmikulicic/test-ci',
+    license='GPL',
+    version='2.0.1',
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: System :: Shells',
+    ],
+)
+
